9e89f49c3f183dba148d3caf127153601d98d5dc
Federico Paganetto <fpaganetto@intouchhealth.com>
2020-08-18 14:35:53 -0300

    Add project files.

---
+++ Dapr.Demo.PS/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dapr.Demo.PS
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public string Subject { get; set; }
+    }
+}
---
+++ Dapr.Demo.PS/MessageController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Dapr.Demo.PS
+{
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly ILogger<MessageController> _logger;
+        public MessageController(ILogger<MessageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("api/message")]
+        public async Task<IActionResult> ReceiveMessage([FromBody] Message message)
+        {
+            _logger.LogInformation($"Message with id {message.Id.ToString()} received!");
+
+            //Validate message received
+            using (var httpClient = new HttpClient())
+            {
+                var result = await httpClient.PostAsync(
+                   "http://localhost:3500/v1.0/publish/messagetopic",
+                   new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json")
+                   );
+
+                _logger.LogInformation($"Message with id {message.Id.ToString()} published with status {result.StatusCode}!");
+            }
+
+            return Ok();
+        }
+
+        [Topic("messagetopic")]
+        [HttpPost]
+        [Route("messagetopic")]
+        public async Task<IActionResult> ProcessOrder([FromBody] Message message)
+        {
+            //Process message placeholder
+            _logger.LogInformation($"Message with id {message.Id.ToString()} processed!");
+            return Ok();
+        }
+    }
+}
